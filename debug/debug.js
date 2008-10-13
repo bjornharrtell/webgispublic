826bd385ceeb4695d1b82d5afca0f9a1c824fd65
bjorn.harrtell <bjorn.harrtell@03a588c4-b548-0410-881d-a38c3de1b75e>
2008-10-13 03:22:58 +0000

    Added separate builds for debug and examples

---
+++ debug/debug.js
@@ -0,0 +1,88 @@
+Ext.namespace('Example');
+
+Example.Application = function() {
+	var map;
+	var toolbar = new Ext.Toolbar();
+
+	var mapOptions = {
+		maxResolution: 1.40625/2,
+		controls: []
+	};
+
+	var layers = [
+	    new OpenLayers.Layer.WMS(
+			"Metacarta Tile Cache",
+			"http://labs.metacarta.com/wms-c/Basic.py?", {
+				"layers": "basic",
+				"format": "image/jpeg"
+			}, {
+				buffer: 0
+			}
+		)
+	];
+
+	return {
+		init: function() {
+
+			Ext.QuickTips.init();
+		
+			var panel = new Ext.Panel({
+				border: false,
+				layout: 'fit',
+				tbar: toolbar
+			});
+			
+			new Ext.Viewport({
+				layout: 'fit',
+				items: panel
+			});
+
+			map = new OpenLayers.Map(panel.body.dom, mapOptions);
+			map.addLayers(layers);
+			map.zoomToMaxExtent();
+
+			var editlayer = new OpenLayers.Layer.Vector('editlayer');
+			map.addLayer(editlayer);
+			
+			// map action is an extended Ext.Action that can be used as a button or menu item
+			toolbar.add(new WebGIS.MapAction.DragPan({map: map}));
+			toolbar.add(new WebGIS.MapAction.ZoomInBox({map: map}));
+			toolbar.add(new WebGIS.MapAction.ZoomOutBox({map: map}));
+			toolbar.add(new WebGIS.MapAction.ZoomIn({map: map}));
+			toolbar.add(new WebGIS.MapAction.ZoomOut({map: map}));
+			toolbar.add(new WebGIS.MapAction.PreviousExtent({map: map}));
+			toolbar.add(new WebGIS.MapAction.NextExtent({map: map}));
+			toolbar.add(new WebGIS.MapAction.FullExtent({map: map}));
+			toolbar.add(new WebGIS.MapAction.MeasureLine({map: map}));
+			toolbar.add(new WebGIS.MapAction.MeasureArea({map: map}));
+			toolbar.add('-');
+			toolbar.add({xtype: 'webgis-scalelist', map: map});
+			toolbar.add('-');
+			toolbar.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Point'}));
+			toolbar.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Curve'}));
+			toolbar.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Polygon'}));
+			toolbar.add('-');
+			toolbar.add(new WebGIS.MapAction.SelectFeature({map: map, layer: editlayer}));
+			toolbar.add(new WebGIS.MapAction.ModifyFeature({map: map, layer: editlayer}));
+			toolbar.add(new WebGIS.MapAction.DragFeature({map: map, layer: editlayer}));
+
+			var toc = new WebGIS.Toc({map: map, useMetadata: true, autoScroll: true});
+			var window = new Ext.Window({
+				title: 'Layers',
+				border: false,
+				layout: 'fit',
+				width: 200,
+				height: 300,
+				items: toc
+			});
+			window.show();
+			window.setPosition(20,50);
+
+			toc.update();
+			
+			return null;
+		}
+	};
+}();
+
+Ext.onReady(Example.Application.init, Example.Application);
