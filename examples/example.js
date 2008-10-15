cba0bdc8741afcb62856490fd13732c05c61a0e7
bjorn.harrtell <bjorn.harrtell@03a588c4-b548-0410-881d-a38c3de1b75e>
2008-10-15 21:02:08 +0000

    Changed the example to use floating panels for the toolbars

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,8 @@ Ext.namespace('Example');
 
 Example.Application = function() {
 	var map;
-	var toolbar = new Ext.Toolbar();
+	var toolbar1 = new Ext.Toolbar();
+	var toolbar2 = new Ext.Toolbar();
 
 	var mapOptions = {
 		maxResolution: 1.40625/2,
@@ -28,8 +29,7 @@ Example.Application = function() {
 		
 			var panel = new Ext.Panel({
 				border: false,
-				layout: 'fit',
-				tbar: toolbar
+				layout: 'fit'
 			});
 			
 			new Ext.Viewport({
@@ -44,27 +44,48 @@ Example.Application = function() {
 			var editlayer = new OpenLayers.Layer.Vector('editlayer');
 			map.addLayer(editlayer);
 			
+			var toolbarWindow1 = new Ext.Window({
+				title: 'Basic tools',
+				border: false,
+				width: 450,
+				autoHeight: true,
+				items: toolbar1
+			});
+			toolbarWindow1.show();
+			toolbarWindow1.setPosition(20,5);
+			
+			var toolbarWindow2 = new Ext.Window({
+				title: 'Edit tools',
+				border: false,
+				width: 200,
+				autoHeight: true,
+				items: toolbar2
+			});
+			toolbarWindow2.show();
+			toolbarWindow2.setPosition(480,5);
+			
 			// map action is an extended Ext.Action that can be used as a button or menu item
-			toolbar.add(new WebGIS.MapAction.DragPan({map: map}));
-			toolbar.add(new WebGIS.MapAction.ZoomInBox({map: map}));
-			toolbar.add(new WebGIS.MapAction.ZoomOutBox({map: map}));
-			toolbar.add(new WebGIS.MapAction.ZoomIn({map: map}));
-			toolbar.add(new WebGIS.MapAction.ZoomOut({map: map}));
-			toolbar.add(new WebGIS.MapAction.PreviousExtent({map: map}));
-			toolbar.add(new WebGIS.MapAction.NextExtent({map: map}));
-			toolbar.add(new WebGIS.MapAction.FullExtent({map: map}));
-			toolbar.add(new WebGIS.MapAction.MeasureLine({map: map}));
-			toolbar.add(new WebGIS.MapAction.MeasureArea({map: map}));
-			toolbar.add('-');
-			toolbar.add({xtype: 'webgis-scalelist', map: map});
-			toolbar.add('-');
-			toolbar.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Point'}));
-			toolbar.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Curve'}));
-			toolbar.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Polygon'}));
-			toolbar.add('-');
-			toolbar.add(new WebGIS.MapAction.SelectFeature({map: map, layer: editlayer}));
-			toolbar.add(new WebGIS.MapAction.ModifyFeature({map: map, layer: editlayer}));
-			toolbar.add(new WebGIS.MapAction.DragFeature({map: map, layer: editlayer}));
+			toolbar1.add('-');
+			toolbar1.add(new WebGIS.MapAction.DragPan({map: map}));
+			toolbar1.add(new WebGIS.MapAction.ZoomInBox({map: map}));
+			toolbar1.add(new WebGIS.MapAction.ZoomOutBox({map: map}));
+			toolbar1.add(new WebGIS.MapAction.ZoomIn({map: map}));
+			toolbar1.add(new WebGIS.MapAction.ZoomOut({map: map}));
+			toolbar1.add(new WebGIS.MapAction.PreviousExtent({map: map}));
+			toolbar1.add(new WebGIS.MapAction.NextExtent({map: map}));
+			toolbar1.add(new WebGIS.MapAction.FullExtent({map: map}));
+			toolbar1.add(new WebGIS.MapAction.MeasureLine({map: map}));
+			toolbar1.add(new WebGIS.MapAction.MeasureArea({map: map}));
+			toolbar1.add('-');
+			toolbar1.add({xtype: 'webgis-scalelist', map: map});
+			toolbar2.add('-');
+			toolbar2.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Point'}));
+			toolbar2.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Curve'}));
+			toolbar2.add(new WebGIS.MapAction.DrawFeature({map: map, layer: editlayer, geometryType: 'OpenLayers.Geometry.Polygon'}));
+			toolbar2.add('-');
+			toolbar2.add(new WebGIS.MapAction.SelectFeature({map: map, layer: editlayer}));
+			toolbar2.add(new WebGIS.MapAction.ModifyFeature({map: map, layer: editlayer}));
+			toolbar2.add(new WebGIS.MapAction.DragFeature({map: map, layer: editlayer}));
 
 			var toc = new WebGIS.Toc({map: map, useMetadata: true, autoScroll: true});
 			var window = new Ext.Window({
@@ -72,11 +93,11 @@ Example.Application = function() {
 				border: false,
 				layout: 'fit',
 				width: 200,
-				height: 300,
+				height: 100,
 				items: toc
 			});
 			window.show();
-			window.setPosition(20,50);
+			window.setPosition(20,70);
 
 			toc.update();
 			
