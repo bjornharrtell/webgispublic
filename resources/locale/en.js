23ccc3863769b3da2ae7d8dca2f8df6d6ecb335c
bjorn.harrtell <bjorn.harrtell@03a588c4-b548-0410-881d-a38c3de1b75e>
2009-04-23 14:32:42 +0000

    Cleanup

--- resources/locale/en.js
+++ resources/locale/en.js
@@ -1,12 +1,10 @@
 /**
- * WebGIS JS Library
- * Copyright(c) 2007, Sweco Position
- *
- * Licensed under GPLv3
- * http://www.gnu.org/licenses/gpl.html
+ * WebGIS JS Library Copyright(c) 2007, Sweco Position
+ * 
+ * Licensed under GPLv3 http://www.gnu.org/licenses/gpl.html
  * 
  * Author: Björn Harrtell
- *
+ * 
  * @fileoverview English locale for all WebGIS public classes
  */
 
@@ -16,82 +14,82 @@ WebGIS.Locale.errorText = "Error message";
 
 // WebGIS.Toc
 Ext.apply(WebGIS.Toc.prototype, {
-	windowTitleMetadataText: 'Metadata for this layer',
-	contextMenuMetadataText: 'Show metadata'
+    windowTitleMetadataText : 'Metadata for this layer',
+    contextMenuMetadataText : 'Show metadata'
 });
 
 // Basic tools
 Ext.apply(WebGIS.MapAction.ZoomInBox.prototype, {
-	titleText: 'Zoom in',
-	tooltipText: 'Zoom in by drawing a rectangle'
+    titleText : 'Zoom in',
+    tooltipText : 'Zoom in by drawing a rectangle'
 });
 Ext.apply(WebGIS.MapAction.ZoomOutBox.prototype, {
-	titleText: 'Zoom out',
-	tooltipText: 'Zoom out by drawing a rectangle'
+    titleText : 'Zoom out',
+    tooltipText : 'Zoom out by drawing a rectangle'
 });
 Ext.apply(WebGIS.MapAction.ZoomIn.prototype, {
-	titleText: 'Zoom in',
-	tooltipText: 'Zoom in one step'
+    titleText : 'Zoom in',
+    tooltipText : 'Zoom in one step'
 });
 Ext.apply(WebGIS.MapAction.ZoomOut.prototype, {
-	titleText: 'Zoom out',
-	tooltipText: 'Zoom out one step'
+    titleText : 'Zoom out',
+    tooltipText : 'Zoom out one step'
 });
 Ext.apply(WebGIS.MapAction.FullExtent.prototype, {
-	titleText: 'Full extent',
-	tooltipText: 'Zoom to the full map extent'
+    titleText : 'Full extent',
+    tooltipText : 'Zoom to the full map extent'
 });
 Ext.apply(WebGIS.MapAction.DragPan.prototype, {
-	titleText: 'Pan',
-	tooltipText: 'Pan by dragging the map'
+    titleText : 'Pan',
+    tooltipText : 'Pan by dragging the map'
 });
 Ext.apply(WebGIS.MapAction.PreviousExtent.prototype, {
-	titleText: 'Previous extent',
-	tooltipText: 'Go to previous extent'
+    titleText : 'Previous extent',
+    tooltipText : 'Go to previous extent'
 });
 Ext.apply(WebGIS.MapAction.NextExtent.prototype, {
-	titleText: 'Next extent',
-	tooltipText: 'Go to next extent'
+    titleText : 'Next extent',
+    tooltipText : 'Go to next extent'
 });
 
 // Editor tools
 Ext.apply(WebGIS.MapAction.DrawFeature.prototype, {
-	titlePointText:		'Draw',
-	tooltipPointText:	'Draw point',
-	titleCurveText:		'Draw',
-	tooltipCurveText:	'Draw line',
-	titlePolygonText:	'Draw',
-	tooltipPolygonText:	'Draw polygon'
+    titlePointText : 'Draw',
+    tooltipPointText : 'Draw point',
+    titleCurveText : 'Draw',
+    tooltipCurveText : 'Draw line',
+    titlePolygonText : 'Draw',
+    tooltipPolygonText : 'Draw polygon'
 });
 Ext.apply(WebGIS.MapAction.SelectFeature.prototype, {
-	titleText: 'Select',
-	tooltipText: 'Select objects'
+    titleText : 'Select',
+    tooltipText : 'Select objects'
 });
 Ext.apply(WebGIS.MapAction.RemoveSelectedFeatures.prototype, {
-	titleText: 'Remove',
-	tooltipText: 'Remove selected objects'
+    titleText : 'Remove',
+    tooltipText : 'Remove selected objects'
 });
 Ext.apply(WebGIS.MapAction.ModifyFeature.prototype, {
-	titleText: 'Modify',
-	tooltipText: 'Modify objects'
+    titleText : 'Modify',
+    tooltipText : 'Modify objects'
 });
 Ext.apply(WebGIS.MapAction.DragFeature.prototype, {
-	titleText: 'Move',
-	tooltipText: 'Move objects'
+    titleText : 'Move',
+    tooltipText : 'Move objects'
 });
 
 // Measure tools
 Ext.apply(WebGIS.MapAction.MeasureLine.prototype, {
-	titleText: 'Measure distance',
-	tooltipText: 'Measure distance by drawing a line'
+    titleText : 'Measure distance',
+    tooltipText : 'Measure distance by drawing a line'
 });
 Ext.apply(WebGIS.MapAction.MeasureArea.prototype, {
-	titleText: 'Measure area',
-	tooltipText: 'Measure distance by drawing a polygon'
+    titleText : 'Measure area',
+    tooltipText : 'Measure distance by drawing a polygon'
 });
 
 // Identify tools
 Ext.apply(WebGIS.MapAction.Identify.prototype, {
-	titleText: 'Identify',
-	tooltipText: 'Shows attributes for an object in the map (only WMS-layers)'
+    titleText : 'Identify',
+    tooltipText : 'Shows attributes for an object in the map (only WMS-layers)'
 });
--- resources/locale/sv.js
+++ resources/locale/sv.js
@@ -1,12 +1,10 @@
 /**
- * WebGIS JS Library
- * Copyright(c) 2007, Sweco Position
+ * WebGIS JS Library Copyright(c) 2007, Sweco Position
  * 
- * Licensed under GPLv3
- * http://www.gnu.org/licenses/gpl.html
- *
+ * Licensed under GPLv3 http://www.gnu.org/licenses/gpl.html
+ * 
  * Author: Björn Harrtell
- *
+ * 
  * @fileoverview Swedish locale for all WebGIS public classes
  */
 
@@ -16,86 +14,86 @@ WebGIS.Locale.errorText = "Felmeddelande";
 
 // WebGIS.Toc
 Ext.apply(WebGIS.Toc.prototype, {
-	windowTitleMetadataText: 'Metadata för lagret',
-	contextMenuMetadataText: 'Visa metadata'
+    windowTitleMetadataText : 'Metadata för lagret',
+    contextMenuMetadataText : 'Visa metadata'
 });
 
 // Basic tools
 Ext.apply(WebGIS.MapAction.ZoomInBox.prototype, {
-	titleText: 'Zooma in',
-	tooltipText: 'Zooma in genom att rita en rektangel'
+    titleText : 'Zooma in',
+    tooltipText : 'Zooma in genom att rita en rektangel'
 });
 Ext.apply(WebGIS.MapAction.ZoomOutBox.prototype, {
-	titleText: 'Zooma ut',
-	tooltipText: 'Zooma ut genom att rita en rektangel'
+    titleText : 'Zooma ut',
+    tooltipText : 'Zooma ut genom att rita en rektangel'
 });
 Ext.apply(WebGIS.MapAction.ZoomIn.prototype, {
-	titleText: 'Zooma in',
-	tooltipText: 'Zooma in ett steg'
+    titleText : 'Zooma in',
+    tooltipText : 'Zooma in ett steg'
 });
 Ext.apply(WebGIS.MapAction.ZoomOut.prototype, {
-	titleText: 'Zooma ut',
-	tooltipText: 'Zooma ut ett steg'
+    titleText : 'Zooma ut',
+    tooltipText : 'Zooma ut ett steg'
 });
 Ext.apply(WebGIS.MapAction.FullExtent.prototype, {
-	titleText: 'Hela kartan',
-	tooltipText: 'Visa hela kartan'
+    titleText : 'Hela kartan',
+    tooltipText : 'Visa hela kartan'
 });
 Ext.apply(WebGIS.MapAction.DragPan.prototype, {
-	titleText: 'Panorera',
-	tooltipText: 'Panorera genom att dra kartan'
+    titleText : 'Panorera',
+    tooltipText : 'Panorera genom att dra kartan'
 });
 Ext.apply(WebGIS.MapAction.PreviousExtent.prototype, {
-	titleText: 'Föregående vy',
-	tooltipText: 'Gå till föregående vy'
+    titleText : 'Föregående vy',
+    tooltipText : 'Gå till föregående vy'
 });
 Ext.apply(WebGIS.MapAction.NextExtent.prototype, {
-	titleText: 'Nästa vy',
-	tooltipText: 'Gå till nästa vy'
+    titleText : 'Nästa vy',
+    tooltipText : 'Gå till nästa vy'
 });
 Ext.apply(WebGIS.MapAction.NextExtent.prototype, {
-	titleText: 'Nästa vy',
-	tooltipText: 'Gå till nästa vy'
+    titleText : 'Nästa vy',
+    tooltipText : 'Gå till nästa vy'
 });
 
 // Editor tools
 Ext.apply(WebGIS.MapAction.DrawFeature.prototype, {
-	titlePointText:		'Rita',
-	tooltipPointText:	'Rita punkt',
-	titleCurveText:		'Rita',
-	tooltipCurveText:	'Rita linje',
-	titlePolygonText:	'Rita',
-	tooltipPolygonText:	'Rita yta'
+    titlePointText : 'Rita',
+    tooltipPointText : 'Rita punkt',
+    titleCurveText : 'Rita',
+    tooltipCurveText : 'Rita linje',
+    titlePolygonText : 'Rita',
+    tooltipPolygonText : 'Rita yta'
 });
 Ext.apply(WebGIS.MapAction.SelectFeature.prototype, {
-	titleText: 'Markera',
-	tooltipText: 'Markera objekt'
+    titleText : 'Markera',
+    tooltipText : 'Markera objekt'
 });
 Ext.apply(WebGIS.MapAction.RemoveSelectedFeatures.prototype, {
-	titleText: 'Ta bort',
-	tooltipText: 'Ta bort markerade objekt'
+    titleText : 'Ta bort',
+    tooltipText : 'Ta bort markerade objekt'
 });
 Ext.apply(WebGIS.MapAction.ModifyFeature.prototype, {
-	titleText: 'Ändra',
-	tooltipText: 'Ändra objekt'
+    titleText : 'Ändra',
+    tooltipText : 'Ändra objekt'
 });
 Ext.apply(WebGIS.MapAction.DragFeature.prototype, {
-	titleText: 'Flytta',
-	tooltipText: 'Flytta objekt'
+    titleText : 'Flytta',
+    tooltipText : 'Flytta objekt'
 });
 
 // Measure tools
 Ext.apply(WebGIS.MapAction.MeasureLine.prototype, {
-	titleText: 'Avståndsmätning',
-	tooltipText: 'Mät avstånd genom att rita en linje'
+    titleText : 'Avståndsmätning',
+    tooltipText : 'Mät avstånd genom att rita en linje'
 });
 Ext.apply(WebGIS.MapAction.MeasureArea.prototype, {
-	titleText: 'Areamätning',
-	tooltipText: 'Mät area genom att rita en polygon'
+    titleText : 'Areamätning',
+    tooltipText : 'Mät area genom att rita en polygon'
 });
 
 // Identify tools
 Ext.apply(WebGIS.MapAction.Identify.prototype, {
-	titleText: 'Identifiera',
-	tooltipText: 'Visar attribut för geometri från en punkt i kartan (endast WMS-lager)'
+    titleText : 'Identifiera',
+    tooltipText : 'Visar attribut för geometri från en punkt i kartan (endast WMS-lager)'
 });
